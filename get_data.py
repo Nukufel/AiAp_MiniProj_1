dcb660782c4165311881a55d87eb0efc611747f4
pantherinblack <stupark@bzz.ch>
2026-03-15 14:36:16 +0100

    feat: data fetching

--- get_data.py
+++ get_data.py
@@ -1,6 +1,21 @@
-from huggingface_hub import snapshot_download
-import zipfile
+from keras.utils import image_dataset_from_directory
+from tensorflow.data.experimental import cardinality
 
-snapshot_download(repo_id="nateraw/rice-image-dataset", repo_type="dataset", local_dir="raw_dataset")
-with zipfile.ZipFile("raw_dataset/rice-image-dataset.zip", "r") as zip_ref:
-    zip_ref.extractall("dataset")
+RAW_DATASET_CACHE = ".cache/extracted/Rice_Image_Dataset"
+SEED = 96
+
+def get_data(image_size = (32, 32)):
+    (train_data, test_data) = image_dataset_from_directory(
+        RAW_DATASET_CACHE,
+        image_size=image_size,
+        validation_split=0.3,
+        subset="both",
+        seed=SEED
+    )
+    train_size = cardinality(train_data).numpy()
+    test_size = cardinality(test_data).numpy()
+
+    train_size = train_data.shuffle(10000, seed=SEED).take(int(train_size * 0.3))
+    test_size = test_data.shuffle(10000, seed=SEED).take(int(test_size * 0.3))
+
+    return train_data, test_data
