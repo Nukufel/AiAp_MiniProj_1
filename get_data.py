6a60ae8c8b23610ef4c73fa10db812ee33796c66
pantherinblack <stupark@bzz.ch>
2026-03-31 16:00:08 +0200

    feat: test model improvements

--- get_data.py
+++ get_data.py
@@ -1,10 +1,10 @@
-from keras.utils import image_dataset_from_directory
+from keras.utils import image_dataset_from_directory, split_dataset
 from tensorflow.data.experimental import cardinality
 
 RAW_DATASET_CACHE = ".cache/extracted/Rice_Image_Dataset"
 SEED = 96
 
-def get_data(image_size = (32, 32)):
+def get_data(image_size):
     (train_data, test_data) = image_dataset_from_directory(
         RAW_DATASET_CACHE,
         image_size=image_size,
@@ -12,10 +12,13 @@ def get_data(image_size = (32, 32)):
         subset="both",
         seed=SEED
     )
+
+    label_names = train_data.class_names
+
     train_size = cardinality(train_data).numpy()
     test_size = cardinality(test_data).numpy()
 
-    train_size = train_data.shuffle(10000, seed=SEED).take(int(train_size * 0.3))
-    test_size = test_data.shuffle(10000, seed=SEED).take(int(test_size * 0.3))
+    train_samples = train_data.shuffle(10000, seed=SEED).take(int(train_size * 0.25))
+    validation_samples = test_data.shuffle(10000, seed=SEED).take(int(test_size * 0.25))
 
-    return train_data, test_data
+    return train_samples, validation_samples, label_names
