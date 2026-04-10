211b05031e5453f1b5f8a549b7c2d42806358962
pantherinblack <kevin.stupar@gmail.com>
2026-04-10 12:44:59 +0200

    feat: removed most Karacadag images and reworked data loading

--- helper.py
+++ helper.py
@@ -1,27 +1,40 @@
 from keras.utils import image_dataset_from_directory
 from sklearn.metrics import ConfusionMatrixDisplay, f1_score, precision_score, recall_score
 from matplotlib import pyplot as plt
-from numpy import ceil, floor
 
 RAW_DATASET_CACHE = ".cache/extracted/Rice_Image_Dataset"
 SEED = 96
 BATCH_SIZE = 32
-NUM_OF_IMAGES = 20000
-TRAIN_SPLIT = 0.7 * 0.8
-VALIDATION_SPLIT = 0.7 * 0.2
+
 TEST_SPLIT = 0.3
+TRAIN_TO_VALIDATION_SPLIT = 0.8
 
-NUMBER_OF_BATCHES = NUM_OF_IMAGES // BATCH_SIZE
+TRAIN_VALIDATION_SPLIT = 1 - TEST_SPLIT
 
-NUMBER_OF_TRAIN_BATCHES = ceil(NUMBER_OF_BATCHES * TRAIN_SPLIT) # ceil because of floating point precision error
-NUMBER_OF_VALIDATION_BATCHES = floor(NUMBER_OF_BATCHES * VALIDATION_SPLIT)
-NUMBER_OF_TEST_BATCHES = floor(NUMBER_OF_BATCHES * TEST_SPLIT)
+TRAIN_SPLIT = TRAIN_VALIDATION_SPLIT * TRAIN_TO_VALIDATION_SPLIT
+VALIDATION_SPLIT = TRAIN_VALIDATION_SPLIT * (1 - TRAIN_TO_VALIDATION_SPLIT)
+
+
+def split_dataset(dataset, train_split, validation_split, test_split):
+    dataset_size = dataset.cardinality().numpy()
+
+    train_size = int(train_split * dataset_size)
+    validation_size = int(validation_split * dataset_size)
+    test_size = int(test_split * dataset_size)
+
+    train_samples = dataset.take(train_size)
+    validation_test_samples = dataset.skip(train_size)
+    validation_samples = validation_test_samples.take(validation_size)
+    test_samples = validation_test_samples.skip(validation_size).take(test_size)
+
+    return train_samples, validation_samples, test_samples
 
 def get_data(image_size):
     data = image_dataset_from_directory(
         RAW_DATASET_CACHE,
         image_size=image_size,
-        batch_size=BATCH_SIZE,
+        # For splitting we want to have it as accurate as possible
+        batch_size=None,
         verbose=False
     )
 
@@ -29,12 +42,20 @@ def get_data(image_size):
 
     data = data.shuffle(10000, seed=SEED)
 
-    train_samples = data.take(NUMBER_OF_TRAIN_BATCHES)
-    validation_samples = data.skip(NUMBER_OF_TRAIN_BATCHES).take(NUMBER_OF_VALIDATION_BATCHES)
-    test_samples = data.skip(NUMBER_OF_TRAIN_BATCHES + NUMBER_OF_VALIDATION_BATCHES).take(NUMBER_OF_TEST_BATCHES)
 
-    return train_samples, validation_samples, test_samples, label_names
+    train_samples, validation_samples, test_samples = split_dataset(
+        data,
+        TRAIN_SPLIT,
+        VALIDATION_SPLIT,
+        TEST_SPLIT,
+    )
 
+    print("Number of training images: ", len(train_samples))
+    print("Number of validation images: ", len(validation_samples))
+    print("Number of testing images: ", len(test_samples))
+    print("Class names: ", label_names)
+
+    return train_samples.batch(BATCH_SIZE), validation_samples.batch(BATCH_SIZE), test_samples.batch(BATCH_SIZE), label_names
 
 def plot_samples(train_images, label_names):
     plt.figure(figsize=(16, 4))
@@ -65,6 +86,7 @@ def plot_accuracy_and_loss(accuracy, validation_accuracy, loss, validation_loss)
     plt.legend()
     plt.show()
 
+
 def calculate_predictions(model, test_images):
     true = []
     pred = []
@@ -74,6 +96,7 @@ def calculate_predictions(model, test_images):
         true.extend(labels)
         pred.extend(model.predict(images, verbose=0).argmax(axis=1))
     return true, pred
+
 
 def plot_confusion_matrix(true, pred, label_names):
     plt.figure(figsize=(10, 20))
--- setup.py
+++ setup.py
@@ -1,5 +1,30 @@
 import kagglehub
+from os import remove, mkdir
+from os.path import exists
+import shutil
 
-# Download latest version
-path = kagglehub.dataset_download("muratkokludataset/rice-image-dataset", output_dir=".cache/extracted")
+DOWNLOAD_PATH = ".cache/extracted"
+EXTRACTED_PATH = DOWNLOAD_PATH + "/Rice_Image_Dataset"
+IMAGE_LABELS = ["Arborio", "Basmati", "Ipsala", "Jasmine", "Karacadag"]
 
+if exists(DOWNLOAD_PATH):
+    shutil.rmtree(DOWNLOAD_PATH)
+
+mkdir(DOWNLOAD_PATH)
+
+kagglehub.dataset_download("muratkokludataset/rice-image-dataset", output_dir=DOWNLOAD_PATH)
+
+shutil.rmtree(f"{DOWNLOAD_PATH}/.complete")
+
+for label in IMAGE_LABELS:
+    for i in range(4001, 15001) if label != "Karacadag" else range(151, 15001):
+        path = f"{EXTRACTED_PATH}/{label}/{label} ({i}).jpg"
+        # Basmati dataset has some files in lowercase
+        path_alt = f"{EXTRACTED_PATH}/{label}/{label.lower()} ({i}).jpg"
+        if exists(path):
+            print(f"Deleting {label}-{i}")
+            remove(path)
+
+        if exists(path_alt):
+            print(f"Deleting {label}-{i}")
+            remove(path_alt)
